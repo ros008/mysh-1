8ee37002d4551fb285819a17089a838e67f81adb
Kim Sujin <ros008@naver.com>
2017-11-09 17:23:19 +0900

    Signal handler complete!

--- src/signal_handlers.c
+++ src/signal_handlers.c
@@ -1,11 +1,14 @@
 #include "signal_handlers.h"
+#include <signal.h>
 
 void catch_sigint(int signalNo)
 {
   // TODO: File this!
+	signal(signalNo, SIG_IGN);
 }
 
 void catch_sigtstp(int signalNo)
 {
   // TODO: File this!
+	signal(signalNo, SIG_IGN);
 }
