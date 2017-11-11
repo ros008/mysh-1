aad810c9128d2eb4ecdb58f7388ad79674322254
Kim Sujin <ros008@naver.com>
2017-11-12 04:38:19 +0900

    Adjust for signal_handlers

--- src/main.c
+++ src/main.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 
 #include "commands.h"
 #include "built_in.h"
 #include "utils.h"
+#include "signal_handlers.h"
 
 int main()
 {
-  char buf[8096];
+  	char buf[8096];
 
-  while (1) {
-    fgets(buf, 8096, stdin);
+  	while (1) {
 
-    struct single_command commands[512];
-    int n_commands = 0;
-    mysh_parse_command(buf, &n_commands, &commands);
+		catch_sigint(SIGINT);
+		catch_sigtstp(SIGTSTP);
 
-    int ret = evaluate_command(n_commands, &commands);
+    		fgets(buf, 8096, stdin);
 
-    free_commands(n_commands, &commands);
-    n_commands = 0;
+    		struct single_command commands[512];
+    		int n_commands = 0;
+    		mysh_parse_command(buf, &n_commands, &commands);
 
-    if (ret == 1) {
-      break;
-    }
-  }
+    		int ret = evaluate_command(n_commands, &commands);
+
+    		free_commands(n_commands, &commands);
+    		n_commands = 0;
+
+    		if (ret == 1) {
+      			break;
+    		}
+  	}
 
   return 0;
 }
